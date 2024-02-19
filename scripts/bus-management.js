e8f8f0f7086ca236077ad2656dc6ccac85b23a03
sumon06 <ohidulislamsumon06@gmail.com>
2024-02-19 12:42:16 +0600

    scrolling

--- scripts/bus-management.js
+++ scripts/bus-management.js
@@ -1,83 +1,71 @@
 // seat related operation (maximum)
 function selectSeat(event) {
-    const findButton = document.getElementById(event);
-    const buttonText = findButton.innerText;
-    console.log(buttonText);
+  const findButton = document.getElementById(event);
+  const buttonText = findButton.innerText;
+  console.log(buttonText);
 
+  // Check this Seat is already select or not
+  const checkArray = [];
+  const checkChildren = document.getElementById("target-section");
+  const checkChildrenValue = checkChildren.getElementsByTagName("p");
+  for (const child of checkChildrenValue) {
+    checkArray.push(child.innerText);
+  }
 
-    // Check this Seat is already select or not
-    const checkArray = [];
-    const checkChildren = document.getElementById('target-section');
-    const checkChildrenValue = checkChildren.getElementsByTagName('p');
-    for(const child of checkChildrenValue){
-        checkArray.push(child.innerText);
-    }
+  // call find the number of innerChild function to get value
+  const numberOfElement = innerChildNumber("target-section");
 
-    // call find the number of innerChild function to get value
-    const numberOfElement = innerChildNumber('target-section');
-   
+  // condition check
+  if (numberOfElement <= 3 && checkArray.includes(buttonText) !== true) {
+    // call the function and append child
+    appendNewChild("target-section", buttonText);
 
-    // condition check
-    if (numberOfElement <= 3 && checkArray.includes(buttonText) !== true) {
+    // Button background collor set
+    setBackgroundColorById(buttonText);
 
-        // call the function and append child
-        appendNewChild('target-section', buttonText);
+    // set total price
+    setTotalPrice("total-price", numberOfElement);
 
-        // Button background collor set
-        setBackgroundColorById(buttonText);
+    // set grand Total price
+    grandTotalPrice("grand-total-price", "total-price");
 
-        // set total price
-        setTotalPrice('total-price', numberOfElement);
+    // decrease total seat
+    totalSeatDecrease("total-seat-count");
 
-        // set grand Total price
-        grandTotalPrice('grand-total-price', 'total-price')
+    // increase select seat
+    increaseSelectSeat("select-seat-number");
 
-        // decrease total seat
-        totalSeatDecrease('total-seat-count');
+    // Enable Net button
+    const submitInformation = document.getElementById("submit-information");
+    submitInformation.removeAttribute("disabled");
 
-        // increase select seat
-        increaseSelectSeat('select-seat-number');
-
-        // Enable Net button
-        const submitInformation = document.getElementById('submit-information');
-        submitInformation.removeAttribute('disabled');
-
-        //
-
-        
-    }
-
+    //
+  }
 }
 
-
-
 function cupon() {
+  const getCuponCode = document.getElementById("cupon-code");
+  const CuponCode = getCuponCode.value;
 
-    const getCuponCode = document.getElementById('cupon-code');
-    const CuponCode = getCuponCode.value;
+  // get total price
+  const totalPriceElement = document.getElementById("total-price");
+  let totalPrice = parseInt(totalPriceElement.innerText);
 
-    // get total price
-    const totalPriceElement = document.getElementById('total-price');
-    let totalPrice = parseInt(totalPriceElement.innerText);
+  // get Grand - total price
+  let totalGrandPriceElement = document.getElementById("grand-total-price");
 
-    // get Grand - total price
-    let totalGrandPriceElement = document.getElementById('grand-total-price');
+  // find cupon section
+  const cuponSection = document.getElementById("cupon-section");
 
-    // find cupon section
-    const cuponSection = document.getElementById('cupon-section');
-
-    if (CuponCode == 'NEW15') {
-        const discount = totalPrice * 0.15;
-        totalPrice = totalPrice - discount;
-        totalGrandPriceElement.innerText = totalPrice;
-        cuponSection.classList.add('hidden');
-    }
-
-    else if (CuponCode == 'Couple 20') {
-        const discount = totalPrice * 0.20;
-        totalPrice = totalPrice - discount;
-        totalGrandPriceElement.innerText = totalPrice;
-        cuponSection.classList.add('hidden');
-    }
-
+  if (CuponCode == "NEW15") {
+    const discount = totalPrice * 0.15;
+    totalPrice = totalPrice - discount;
+    totalGrandPriceElement.innerText = totalPrice;
+    cuponSection.classList.add("hidden");
+  } else if (CuponCode == "Couple 20") {
+    const discount = totalPrice * 0.2;
+    totalPrice = totalPrice - discount;
+    totalGrandPriceElement.innerText = totalPrice;
+    cuponSection.classList.add("hidden");
+  }
 }
